594f5ab666d41d8d3609d0e5930b148dca1a66b8
alexandrminskiy <alexandrminskiy@gmail.com>
2023-02-05 16:29:27 +0300

    28 Урок

--- put_samuraya/src/components/Dialogs/Dialogs.jsx
+++ put_samuraya/src/components/Dialogs/Dialogs.jsx
@@ -3,34 +3,20 @@ import s from './Dialogs.module.css'
 import Message from './Message/Message';
 
 const Dialogs = (props) => {
-    let dialogsDate = [
-        { id: 1, name: 'Дима' },
-        { id: 2, name: 'Женя' },
-        { id: 3, name: 'Лена' },
-        { id: 4, name: 'Тусик' },
-    ]
 
-    let messagesDate = [
-        { id: 1, message: 'hello' },
-        { id: 2, message: 'привет' },
-        { id: 3, message: 'как дела' },
-        { id: 4, message: 'что делаешь ?' },
-    ]
+    let dialogsElements = props.dialogsData.map(d => <DialogItem name={d.name} id={d.id} />);
+    let messagesElements = props.messagesData.map(m => <Message message={m.message} />);
 
-    let messagesElement = messagesDate.map(el => <Message message={el.message} />);
-    let dialogsElements = dialogsDate.map(el => <DialogItem name={el.name} id={el.id} />);
-
-    // console.log(el);
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <div className={s.dialogsItems}>
-                    { dialogsElements }
+                    {dialogsElements}
                 </div>
             </div>
 
             <div className={s.messages}>
-                {messagesElement}
+                {messagesElements}
             </div>
         </div>
     )
--- put_samuraya/src/index.js
+++ put_samuraya/src/index.js
@@ -8,10 +8,24 @@ let posts = [
   { id: 2, message: 'какая хорошая погода сегодня?', likesCount: 11},
 ]
 
+let dialogsData = [
+  { id: 1, name: 'Дима' },
+  { id: 2, name: 'Женя' },
+  { id: 3, name: 'Лена' },
+  { id: 4, name: 'Тусик' },
+]
+
+let messagesData = [
+  { id: 1, message: 'hello' },
+  { id: 2, message: 'привет' },
+  { id: 3, message: 'как дела' },
+  { id: 4, message: 'что делаешь ?' },
+]
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App posts={posts}/>
+    <App posts={posts} dialogsData={dialogsData} messagesData={messagesData} />
     
   </React.StrictMode>
 );
